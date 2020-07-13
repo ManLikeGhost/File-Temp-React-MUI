04fe716d6b31a98985b5c2fd97e1ed50b83ecc8d
Ridwan <balogunridwan@gmail.com>
2020-07-13 15:31:13 -0700

    Added logo and Marble background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import MarbleBackground from './img/MarbleBackground.png';
+import Button from '@material-ui/core/Button';
 import './App.css';
+
+const styles = {
+    paperContainer: {
+        height: "100vh",
+        backgroundImage: `url(${MarbleBackground})`
+    }
+};
 
 function App() {
     return (
-    <div className="App" > 
-    Welcome to Terrel Davies</div>
+    <div style={styles.paperContainer}> 
+    
+    <Button variant="contained">Welcome to Terrel Davies</Button>
+    </div>
         
     );
 }
