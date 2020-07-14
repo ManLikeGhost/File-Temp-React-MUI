6b2bba19516db7e93c3a222e9d1fb8460f61a21a
Ridwan Balogun <balogunridwan@gmail.com>
2020-07-14 13:04:38 +0100

    Add Navigation header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import MarbleBackground from './img/MarbleBackground.png';
-import Button from '@material-ui/core/Button';
-import './App.css';
+import React from "react";
+import MarbleBackground from "./img/MarbleBackground.png";
+import Header from "./components/header/header";
+import Button from "@material-ui/core/Button";
+import "./App.css";
 
 const styles = {
-    paperContainer: {
-        height: "100vh",
-        backgroundImage: `url(${MarbleBackground})`
-    }
+  paperContainer: {
+    height: "100vh",
+    backgroundImage: `url(${MarbleBackground})`,
+  },
 };
 
 function App() {
-    return (
-    <div style={styles.paperContainer}> 
-    
-    <Button variant="contained">Welcome to Terrel Davies</Button>
+  return (
+    <div style={styles.paperContainer}>
+      <Header />
+      <Button variant="contained"> Welcome to Terrel Davies... </Button>
     </div>
-        
-    );
+  );
 }
 
 export default App;
