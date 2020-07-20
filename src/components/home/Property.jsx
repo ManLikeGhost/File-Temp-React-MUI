72fbbb30cf35a43aa41f0fa66499c3c0c1bd0986
Ridwan Balogun <balogunridwan@gmail.com>
2020-07-20 17:46:20 +0100

    Worked on Property

--- src/components/home/Property.jsx
+++ src/components/home/Property.jsx
@@ -4,22 +4,33 @@ import PropertyFeatures from "./propertyFeatures";
 //MUI
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
+import Paper from "@material-ui/core/Paper";
+
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
-import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-import Divider from '@material-ui/core/Divider';
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Divider from "@material-ui/core/Divider";
 
-const useStyles = makeStyles(theme=>( {
+const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 345,
   },
   cardColor: {
     backgroundColor: "#F5E9DE",
-  }
+  },
+  divider: {
+    margin: "20px 0",
+    backgroundColor: theme.palette.primary.main,
+    height: "1px",
+  },
+  location: {
+    display: "flex",
+  },
+  locationIcon: {
+    alignSelf: "flex-end",
+  },
 }));
 
 export default function Property({ imagePath, saleText, address, details }) {
@@ -27,16 +38,20 @@ export default function Property({ imagePath, saleText, address, details }) {
 
   return (
     <Card className={classes.root}>
-      
-        <CardMedia
-          component="img"
-          alt={saleText}
-          height="300"
-          image={imagePath}
-          title={saleText}
-        />
-        <CardContent  className={classes.cardColor}>
-          <Grid container>
+      <CardMedia
+        component="img"
+        alt={saleText}
+        height="300"
+        image={imagePath}
+        title={saleText}
+      />
+
+      <CardContent className={classes.cardColor}>
+        <Grid container>
+          <Grid item xs={2}>
+            <LocationOnOutlinedIcon />
+          </Grid>
+          <Grid container xs={10}>
             <Grid item xs={12}>
               <Typography
                 gutterBottom
@@ -48,26 +63,15 @@ export default function Property({ imagePath, saleText, address, details }) {
               </Typography>
             </Grid>
             <Grid item xs={12}>
-              <Grid container>
-                <Grid item xs={2}>
-                  <LocationOnOutlinedIcon />
-                </Grid>
-                <Grid item xs={10}>
-                  <Typography variant="body2" component="p" color="secondary">
-                    {address}
-                  </Typography>
-                </Grid>
-              </Grid>
+              <Typography variant="body2" component="p" color="secondary">
+                {address}
+              </Typography>
             </Grid>
           </Grid>
-          <Divider variant="middle" />
-          <PropertyFeatures />
-        </CardContent>
-
-      
-      <CardActions className={classes.cardColor}>
-       
-      </CardActions>
+        </Grid>
+        <Divider variant="middle" className={classes.divider} />
+        <PropertyFeatures />
+      </CardContent>
     </Card>
   );
 }
