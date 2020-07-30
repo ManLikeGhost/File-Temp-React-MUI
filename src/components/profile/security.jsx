22b7517e4ea6acfc0f0f34b300733250846bb9cc
Ridwan Balogun <balogunridwan@gmail.com>
2020-07-30 13:39:17 +0100

    Added saved property page

--- src/components/profile/security.jsx
+++ src/components/profile/security.jsx
@@ -1,14 +1,25 @@
 
-
-
 import React from "react";
+import ButtonAccount from "./miniComponents/button";
+import MessageAccount from "./miniComponents/message";
+import ProfileSectionTitle from "./miniComponents/profilesectionTitle";
 
-function Security() {
+import "./content.css";
+
+const Security = () => {
   return (
     <div>
-      <h1>Security</h1>
+      <ProfileSectionTitle>
+        <div>Saved Property</div>
+        <div>Manage your subscription plans</div>
+      </ProfileSectionTitle>
+      <div className="content">
+        <MessageAccount>You have no saved property on this page</MessageAccount>
+        <ButtonAccount>ADD PROPERTY</ButtonAccount>
+      </div>
     </div>
   );
-}
+};
 
 export default Security;
+
