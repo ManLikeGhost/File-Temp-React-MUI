d51d2db82ee937b6cc27d4847879d9e4e69723a3
Ridwan Balogun <balogunridwan@gmail.com>
2020-08-06 17:15:03 +0100

    Developed Upgrade subscription page

--- src/components/subscriptionPlan/upgradeSubscription.jsx
+++ src/components/subscriptionPlan/upgradeSubscription.jsx
@@ -9,22 +9,38 @@ const useStyles = makeStyles((theme) => ({
     border: "1px solid #BF7950",
     boxSizing: "border-box",
     borderRadius: "2px",
+    paddingBottom: "50px",
   },
   title: {
     background: theme.palette.primary.main,
     color: "white",
     padding: theme.spacing(1, 2),
     marginBottom: theme.spacing(5),
+    fontSize: "1.1rem",
   },
+  titleSpan: {
+    fontSize: "1.3rem",
+    fontWeight: "bold",
+  },
+
   listingContainer: {
-    padding: theme.spacing(1, 2),
-    marginBottom: theme.spacing(5),
+    paddingLeft: theme.spacing(5),
+    // marginBottom: theme.spacing(4),
+    textAlign: "left",
   },
   divider: {
-    // border: "1px solid #BF7950",
-    margin: "20px 0",
-    backgroundColor: theme.palette.primary.main,
-    height: "1px",
+    border: "1px solid #BF7950",
+    margin: theme.spacing(6, 0),
+
+    width: "90%",
+    background: theme.palette.primary.main,
+  },
+  text: {
+    color: "rgba(0, 0, 0, 0.51)",
+    fontSize: "1.2rem",
+  },
+  textValue: {
+    color: theme.palette.primary.main,
   },
 }));
 
@@ -32,30 +48,53 @@ const UpgradeSubscription = () => {
   const classes = useStyles();
   return (
     <div>
-      <Grid container className={classes.container}>
+      <Grid
+        container
+        className={classes.container}
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
         <Grid item xs={12} className={classes.title}>
           <Typography>
-            Current Subscription Status: <span>STANDARD</span>
+            Current Subscription Status: <span className={classes.titleSpan} >STANDARD</span>
           </Typography>
         </Grid>
-        <Grid container className={classes.listingContainer}>
+        <Grid
+          container
+          className={classes.listingContainer}
+          direction="row"
+          justify="space-evenly"
+          alignItems="center"
+        >
           <Grid item xs={4}>
-            Listings: {5}
+            <Typography className={classes.text}>
+              Listings: <span className={classes.textValue}>{5}</span>
+            </Typography>
           </Grid>
           <Grid item xs={4}>
-            Premium Listings: {0}
+            <Typography className={classes.text}>
+              Premium Listings: <span className={classes.textValue}>{0}</span>
+            </Typography>
           </Grid>
           <Grid item xs={4}>
-            Featured Ad Listings: 0
+            <Typography className={classes.text}>
+              Featured Ad Listings:{" "}
+              <span className={classes.textValue}>{0}</span>
+            </Typography>
           </Grid>
         </Grid>
 
         <Divider variant="middle" className={classes.divider} />
-        <Grid item xs={4}>
-          Expiry Date: 0
+        <Grid container className={classes.listingContainer}>
+          <Grid item xs={4}>
+            <Typography className={classes.text}>
+              Expiry Date:<span className={classes.textValue}>{0}</span>{" "}
+            </Typography>
+          </Grid>
         </Grid>
       </Grid>
-      <Grid container></Grid>
+
       <Grid container>
         <Grid item xs={12}>
           <Typography>
