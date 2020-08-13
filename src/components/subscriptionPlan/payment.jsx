9b702d9507543689980e4adf3eba7d0bffe8344a
Ridwan Balogun <balogunridwan@gmail.com>
2020-08-13 15:35:02 +0100

    Develop payment component

--- src/components/subscriptionPlan/payment.jsx
+++ src/components/subscriptionPlan/payment.jsx
@@ -9,34 +9,58 @@ import { makeStyles } from "@material-ui/core/styles";
 import Logo from "../../img/logo.png";
 import PaystackLogo from "../../img/paystack-logo.png";
 
-
 const useStyles = makeStyles((theme) => ({
-    root: {
-      background: "#FFFFFF",
-      border: "1px solid #BF7950",
-      boxSizing: "border-box",
-      borderRadius: "2px",
-      // paddingBottom: "50px",
-      marginTop: theme.spacing(2),
-      padding: theme.spacing(6),
-    },
-}))
+  root: {
+    background: "#FFFFFF",
+    border: "1px solid #BF7950",
+    boxSizing: "border-box",
+    borderRadius: "2px",
+    // paddingBottom: "50px",
+    marginTop: theme.spacing(2),
+    padding: theme.spacing(6),
+  },
+  text: {
+    color: "rgba(0, 0, 0, 0.51)",
+    fontSize: "1rem",
+  },
+  amount: {
+    color: theme.palette.secondary.main,
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+  },
+  divider: {
+    margin: "40px 0",
+    backgroundColor: theme.palette.primary.main,
+    height: "1px",
+  },
+  info: {
+    color: theme.palette.secondary.main,
+    fontSize: "1.2rem",
+    textAlign: "center",
+    fontWeight: "bold",
+    margin: theme.spacing(1, 0, 8, 0),
+  },
+}));
 
 const payment = () => {
-    const classes = useStyles();
+  const classes = useStyles();
   return (
     <div className={classes.root}>
       <Grid container>
         <Grid item xs={6}>
           <img src={Logo} alt="logo" width="50px" />
         </Grid>
-        <Grid item xs={6} style={{marginLeft: "auto"}}>
-          <Typography>john.doe@xyz.com</Typography>
-          <Typography>Pay N57,000</Typography>
+        <Grid item xs={6} style={{ textAlign: "right" }}>
+          <Typography className={classes.text}>john.doe@xyz.com</Typography>
+          <Typography className={classes.text}>
+            Pay <span className={classes.amount}>N57,000</span>{" "}
+          </Typography>
         </Grid>
       </Grid>
-      <Divider />
-      <Typography>Enter your debit card details to pay</Typography>
+      <Divider className={classes.divider} />
+      <Typography className={classes.info}>
+        Enter your debit card details to pay
+      </Typography>
 
       <Grid container spacing={3}>
         <Grid item xs={12} md={12} sm={12}>
@@ -48,6 +72,7 @@ const payment = () => {
             fullWidth
             autoComplete="cc-number"
             variant="outlined"
+            className={classes.text}
           />
         </Grid>
         <Grid item xs={12} md={6}>
@@ -60,6 +85,7 @@ const payment = () => {
             fullWidth
             autoComplete="cc-exp"
             variant="outlined"
+            className={classes.text}
           />
         </Grid>
         <Grid item xs={12} md={6}>
@@ -72,22 +98,21 @@ const payment = () => {
             fullWidth
             autoComplete="cc-csc"
             variant="outlined"
+            className={classes.text}
           />
         </Grid>
       </Grid>
 
-      <Grid container>
-        <Grid item xs={3}></Grid>
+      <Grid container style={{ marginTop: "50px" }}>
+        <Grid item xs={4}></Grid>
         <Grid item xs={6}>
-          <span>
+          <div style={{ display: "flex" }}>
             <HttpsIcon color="primary" />
-          </span>
-
-          <Typography>Secured by</Typography>
+            <Typography>Secured by</Typography>
+            <img src={PaystackLogo} alt="Paystack logo" width="100px" />
+          </div>
         </Grid>
-        <Grid item xs={3}>
-          <img src={PaystackLogo} alt="Paystack logo" />
-        </Grid>
+        <Grid item xs={2}></Grid>
       </Grid>
     </div>
   );
