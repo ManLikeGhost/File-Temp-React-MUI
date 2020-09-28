c4fb908e1414d54f8fde9616c775c75ffbef4fb1
ManLikeGhost <ekpoabasiama@gmail.com>
2020-09-28 17:18:08 +0100

    blog component refactor and contact-us page finished

---
+++ src/components/blog/BlogProperty.jsx
@@ -0,0 +1,115 @@
+
+import React from 'react'
+
+//import MarbleBackground from "../../img/MarbleBackground.png";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+
+import { makeStyles } from "@material-ui/core/styles";
+
+
+
+const useStyles = makeStyles((theme) => ({
+    backgroundImage: {
+      height: "80vh",
+      //backgroundImage: `url(${MarbleBackground})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      margin: "0px 5px 0px 50px",
+      // width: `calc(100vw + 48px)`,
+    },
+
+    blogpic: {
+        width: "100%",
+        height: "50%",
+    },
+  
+    blogCard: {
+        width: "311px",
+        height: "352px",
+        border: "2px solid #BF7950",
+    },
+  
+    blogArtHold: {
+        padding: "0px 0px 20px 0px",
+    },
+
+    headertext: {
+        width: "260px",
+        height: "25px",
+        fontFamily: "Metropolis",
+        fontStyle: "normal",
+        fontSize: "25px",
+        color: "#BF7950",
+        padding: "0px 0px 10px 20px",
+    },
+  
+    ptext: {
+        width: "269px",
+        height: "40px",
+        fontFamily: "Metropolis",
+        fontStyle: "normal",
+        fontSize: "20px",
+        color: "rgba(0, 0, 0, 0.51)",
+        padding: "10px 0px 10px 20px"
+    },
+  
+      
+    category: {
+        width: "86px",
+        height: "16px",
+        fontFamily: "Metropolis",
+        fontStyle: "normal",
+        fontWeight: "300",
+        fontSize: "16px",
+        color: "#04403A",
+        padding: "60px 0px 0px 10px  ",
+    },
+  
+    date: {
+        padding: "60px 0px 0px 20px  ",
+        float: "right",
+    },
+  
+}));
+
+
+
+const BlogProperty = ({ title, imagePath, text, type, date }) => {
+    const classes = useStyles();
+    return (
+        <div>
+           <Grid container spacing={3} className={classes.blogArtHold}> 
+                <Grid item xs={4}>
+                <div className={classes.blogCard}> 
+                    <img className={classes.blogpic}  alt="House with bule background" src={imagePath}/>
+                    <Grid container>
+                    <Grid item xs={12}><Typography className={classes.headertext}>{title}</Typography></Grid>
+                    <Grid item xs={12}><Typography className={classes.ptext}>{text}</Typography></Grid>
+                    <Grid item xs={4}><Typography className={classes.category}>{type}</Typography></Grid>
+                    <Grid item xs={6}><Typography className={classes.date}>{date}</Typography></Grid>
+                    </Grid>
+                </div>
+                </Grid>
+            </Grid>
+ 
+        </div>
+    )
+}
+
+export default BlogProperty;
+
+
+     
+  
+
+
+
+
+
+
+
+
+
+        
--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -28,6 +28,10 @@ const useStyles = makeStyles((theme) => ({
       // width: `calc(100vw + 48px)`,
     },
 
+    contactUs: {
+        margin: "0px 0px 0px 100px",
+    },
+
     form: {
         width: "1064px",
         height: "1101px",
@@ -38,6 +42,44 @@ const useStyles = makeStyles((theme) => ({
         borderRadius: "2px",
     },
 
+    contactText: {
+        width: "873px",
+        height: "279px",
+        fontFamily: "Playfair Display",
+        fontStyle: "normal",
+        fontWeight: "bold",
+        fontSize: "70px",
+        color: "#BF7950",
+    },
+
+    input: {
+        width: "950px",
+        height: "55px",
+        background: "#FFFFFF",
+        //border: "0.5px solid #BF7950",
+        //boxSizing: "border-box",
+        //borderRadius: "2px",
+    },
+
+    nameInput: {
+        
+        padding: "50px 25px 15px 25px",
+        
+    },
+
+    map: {
+        padding: "20px 0px 0px 0px"
+    },
+
+    //message: {
+        //width: "950px",
+        //height: "466px",
+        //background: "#FFFFFF",
+        //border: "0.5px solid #BF7950",
+        //boxSizing: "border-box",
+        //borderRadius: "2px",
+    //},
+    
 }));
 
 
@@ -48,10 +90,10 @@ const ContactUsPage = () => {
     return (
         <div className={classes.backgroundImage}>
             <NavigationHeader />
-            <div>
+            <div className={classes.contactUs}>
                 <Grid container>
                     <Grid item xs={12}>
-                        <Typography variant="h3">Questions, inquiries...We would love to hear from you. Email us.</Typography>
+                        <Typography className={classes.contactText} variant="h3">Questions, inquiries...We would love to hear from you. Email us.</Typography>
                     </Grid>
 
                     <Grid item xs={12}>
@@ -60,41 +102,53 @@ const ContactUsPage = () => {
                                 <FormControl>
                                     <Grid container>
                                         <Grid item xs={12}>
-                                            <TextField 
-                                                id="name"
-                                                label="Name"
-                                                variant="outlined"
-                                                color="secondary"
-                                            />
+                                            <div className={classes.nameInput}>                                            
+                                                <TextField 
+                                                    id="name"
+                                                    className={classes.input}
+                                                    label="Name"
+                                                    variant="outlined"
+                                                    color="secondary"
+                                                />
+                                            </div>
                                         </Grid>
 
                                         <Grid item xs={12}>
-                                            <TextField 
-                                                id="email"
-                                                label="Email"
-                                                variant="outlined"
-                                                color="secondary"
-                                            />
+                                            <div className={classes.nameInput}>
+                                                <TextField 
+                                                    id="email"
+                                                    className={classes.input}
+                                                    label="Email"
+                                                    variant="outlined"
+                                                    color="secondary"
+                                                />
+                                            </div>
                                         </Grid>
 
                                         <Grid item xs={12}>
-                                            <TextField 
-                                                id="subject"
-                                                label="Subject"
-                                                variant="outlined"
-                                                color="secondary"
-                                            />
+                                            <div className={classes.nameInput}>
+                                                <TextField 
+                                                    id="subject"
+                                                    className={classes.input}
+                                                    label="Subject"
+                                                    variant="outlined"
+                                                    color="secondary"
+                                                />
+                                            </div>    
                                         </Grid>
 
                                         <Grid item xs={12}>
-                                            <TextareaAutosize
-                                                rowsMax={20}
-                                                aria-label="maximum height"
-                                                style={{
-                                                width: "39rem",
-                                                height: "10rem",
-                                                }}
-                                            />
+                                            <div className={classes.nameInput}>
+                                                <TextareaAutosize
+                                                    className={classes.message}
+                                                    rowsMax={20}
+                                                    aria-label="maximum height"
+                                                    style={{
+                                                    width: "950px",
+                                                    height: "466px",
+                                                    }}
+                                                />
+                                            </div>
                                         </Grid>
                                     </Grid>
                                     <Grid container>
@@ -113,9 +167,11 @@ const ContactUsPage = () => {
                         </div>
                 
                     </Grid>
-                        <div>
+                    <Grid item xs={12}>
+                        <div className={classes.map}>
                             <img className={classes.pic} src={contactUs} alt="contactUsMap"/>
                         </div>
+                    </Grid>    
 
                 </Grid>
             </div>
