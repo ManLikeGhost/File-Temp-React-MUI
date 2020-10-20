cf8173ad588b1d94b4824f096893232a48983a7c
Ridwan Balogun <balogunridwan@gmail.com>
2020-10-20 16:42:52 +0100

    About Us Homepage: Added Background Image

--- src/components/home/addListing.jsx
+++ src/components/home/addListing.jsx
@@ -4,12 +4,21 @@ import Link from "@material-ui/core/Link";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
-
+import ListPropertyBackground from "../../img/ListPropertyBackground.png";
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.secondary.main,
-    margin: "50px",
+  // root: {
+  //   backgroundColor: theme.palette.secondary.main,
+  //   margin: "50px",
+  //   height: "100vh",
+  // },
+  listPropertyBackgroundImage: {
     height: "100vh",
+    backgroundImage: `url(${ListPropertyBackground})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+      backgroundColor: theme.palette.secondary.main,
+    margin: "50px",
+    // width: `calc(100vw + 48px)`,
   },
   textStyle: {
     fontStyle: "normal",
@@ -37,7 +46,7 @@ const useStyles = makeStyles((theme) => ({
 const AddListingSection = () => {
   const classes = useStyles();
   return (
-    <div className={classes.root}>
+    <div className={classes.listPropertyBackgroundImage}>
       <Grid container direction="row" justify="center" alignItems="center">
         <Grid item xs={12}>
           <Typography className={classes.textStyle}>
