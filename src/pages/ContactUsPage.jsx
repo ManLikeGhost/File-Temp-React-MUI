ec8adda901f320d0b36d81a6c72da21a8faed399
Ridwan Balogun <balogunridwan@gmail.com>
2020-11-12 15:45:50 +0100

    Override Multiline Input in muiTheme file and changed Text Auto resize to Multiline in Post Property Request and Contact Us page

--- src/pages/ContactUsPage.jsx
+++ src/pages/ContactUsPage.jsx
@@ -8,7 +8,6 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import MapContainer from "../components/contactUs/googleMap";
 import FormControl from "@material-ui/core/FormControl";
-import TextareaAutosize from "@material-ui/core/TextareaAutosize";
 
 import { makeStyles } from "@material-ui/core/styles";
 
