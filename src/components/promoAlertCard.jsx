e73988c035073098e1ad8faeda73f03abcbf5ce0
manlikeghost <ekpoabasiama@gmail.com>
2020-11-17 00:42:31 +0100

    newChanges bootstrapping

---
+++ src/components/promoAlertCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+import { makeStyles } from "@material-ui/core/styles";
+
+
+const useStyles = makeStyles((theme) => ({
+    
+    
+
+}));
+
+const PromoAlertCard = () => {
+    const classes = useStyles();
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PromoAlertCard
